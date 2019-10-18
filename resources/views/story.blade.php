028a5df19f341dcb539d8bf35635c6260c3b0eef
Edbergarn <emil@5CD9244GCX.learnet.se>
2019-10-18 11:13:45 +0200

    fancy

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -2,7 +2,7 @@
 @section('body')
     <div class="box text" style="background-image: linear-gradient(to bottom right, #{{ $color }}, #{{ $color2 }});">
         <h1>UserStory</h1>
-        <h2> {{ $story[0]->story }}</h2>
+        <h2> {{ $story[0]->story ?? "" }}</h2>
         <form action="/" method="get">
             <input class="button" type="submit" value="Skriv en till vettja!">
         </form>
