419fe5d5628860eb35deef5c1684ab6c1316c487
Edbergarn <emil@5CD9244GCX.learnet.se>
2019-10-18 13:07:59 +0200

    fitta v2

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -8,9 +8,8 @@
     <div class="box text" style="background-image: linear-gradient(to bottom right, #{{ $color }}, #{{ $color2 }});">
         <h1>UserStory</h1>
         <h2> {{ $story[0]->story ?? "" }}</h2>
-        {{-- <form action="/" method="get"> --}}
-            {{-- <input class="button" type="submit" value="Skriv en till vettja!"> --}}
-        {{-- </form> --}}
-        <a href="../">hej</a>
+        <form action="/" method="get">
+            <input class="button" type="submit" value="Skriv en till vettja!">
+        </form>
     </div>
 @endsection
